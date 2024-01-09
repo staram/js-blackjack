38e30a2dfca849b63a1d5757b1de0893169cf43e
staram <10952725+staram@users.noreply.github.com>
2024-01-09 01:20:05 +0000

    Replace message text with result text
    getElementbyId, textContent

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 10
 let sum = firstCard + secondCard
 let hasBlackJack = false
+let isAlive = true
+let message = ""
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card? 😁")
-} else if (sum === 21) {
-    console.log("Woohoo! You've got Blackjack!")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game! Goodbye.")
+
+let messageEl = document.getElementById("message-el")
+console.log(messageEl)
+
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    // 2. Display the message in the messageEl using messageEl.textContent
+    messageEl.textContent = message
 }
 
-console.log("Blackjack status: " + hasBlackJack)
